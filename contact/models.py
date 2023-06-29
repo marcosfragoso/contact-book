ac16dfd5bca84fdc26e437dde59d768130b764f5
Marcos <fragoso.ufcg@gmail.com>
2023-06-29 15:43:22 -0300

    add model admin

--- contact/models.py
+++ contact/models.py
@@ -9,4 +9,6 @@ class Contact(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
 
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
 
