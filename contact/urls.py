603bcd505c0c70a7a81128638a2489d88ec5165f
Marcos <fragoso.ufcg@gmail.com>
2023-07-03 16:24:59 -0300

    adiciona validacoes users

--- contact/urls.py
+++ contact/urls.py
@@ -13,4 +13,12 @@ urlpatterns = [
 
     path('search/', views.search, name='search'),
     path('', views.index, name='index'),
+
+
+    path('user/create/', views.register, name='register'),
+    path('user/login/', views.login_view, name='login'),
+    path('user/logout/', views.logout_view, name='logout'),
+    path('user/update/', views.user_update, name='user_update'),
+    
+
 ]
